643ebc24b5fe5acabf67b248c68e2b9edd4b1b04
Jamie Flournoy <jamie@pervasivecode.com>
2012-09-08 20:31:09 -0700

    Made installer work.

--- install.sh
+++ install.sh
@@ -1,23 +1,43 @@
 #!/bin/bash
 
-cd ~/
+cd
 
-function install_file {
-  BASENAME=$1
-  ORIG=".${BASENAME}_orig"
-  DOTFILE=".${BASENAME}"
-  SOURCE="~/myconfigs/dot_${BASENAME}"
-  echo "Preserving '$DOTFILE' as '$ORIG'"
-  /bin/mv "$DOTFILE" "$ORIG"
-  echo "Symlinking '$SOURCE' as '$DOTFILE'"
-  /bin/ln -s "$SOURCE" "$DOTFILE"
+function quit_unless_file_exists {
+    if [ ! -f $1 ]; then
+	echo "Source file '${1}' not found - cannot be installed."
+	exit 1
+    fi
 }
 
-install_file bashrc
-install_file emacs
-install_file gitconfig
-install_file gitignore
-install_file screenrc
+function preserve_file_if_exists {
+    FILE="${1}"
+    SAVE="${1}_orig"
+    if [ -f "${FILE}" ]; then
+	echo "Preserving existing '${FILE}' as '${SAVE}'"
+	# /bin/mv "${FILE}" "${SAVE}"
+    fi
+}
 
+function install_symlink {
+    SOURCE="${HOME}/myconfigs/dot_${1}"
+    quit_unless_file_exists "${SOURCE}"
+    DOTFILE=".${1}"
+    preserve_file_if_exists ".${1}"
+    echo "Symlinking '${SOURCE}' as '${DOTFILE}'"
+    /bin/ln -s "${SOURCE}" "${DOTFILE}"
+}
 
+function install_copy {
+    SOURCE="${HOME}/myconfigs/dot_${1}"
+    quit_unless_file_exists "${SOURCE}"
+    DOTFILE=".${1}"
+    preserve_file_if_exists ".${1}"
+    echo "Copying '${SOURCE}' to '${DOTFILE}'"
+    /bin/cp "${SOURCE}" "${DOTFILE}"
+}
 
+install_symlink bashrc
+install_symlink emacs
+install_copy gitconfig
+install_symlink gitignore
+install_symlink screenrc
